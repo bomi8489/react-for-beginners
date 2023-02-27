006602a826e86a1feb24be8eeeb5002467fa9509
AUTUMN <krshlee22@naver.com>
2023-02-27 23:51:24 +0900

    React_practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { memo, useState, useEffect } from "react";
 
-function App() {
+function Hello() {
+  useEffect(() => {
+    console.log("Created :)");
+    return () => console.log("Destroyed :("); // cleanup function
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Hello</h1>
+  )
+}
+
+const MemorizedHello = memo(Hello)
+function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeword] = useState("");
+  const [showing, setShwoing] = useState(false);
+
+  const onChange = (e) => setKeword(e.target.value);
+  const onClick = () => setCounter(current => current+1);
+  const showBtn = () => setShwoing(prev => !prev);
+
+  // deps가 변화할때 한번 실행, deps가 비어있다면 처음 렌더링시 한번만 실행
+  useEffect(() => {
+    console.log("I run only once.")
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.")
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.")
+  }, [counter]);
+  
+  return (
+    <div>
+      <div>
+        <input 
+          value={keyword} 
+          onChange={onChange} 
+          type="text" 
+          placeholder="Search here..." 
+        />
+        <h1>Count {counter}</h1>
+        <button onClick={onClick}>click me</button>
+      </div>
+      <br /><hr /><br />
+
+      <div>
+        <button onClick={showBtn}>{showing ? "Hide" : "Show"}</button>
+        {showing ? <MemorizedHello /> : null}
+      </div>
     </div>
   );
 }
